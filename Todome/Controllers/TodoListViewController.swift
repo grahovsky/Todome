062c75e24994497a1e1934a98cf3ae14f49ff5a9
Konstantin <grahovsky@gmail.com>
2019-03-09 15:53:02 +0300

    #searchBar textDidChange #resignFirstResponder #DispatchQueue

--- Todome/Controllers/TodoListViewController.swift
+++ Todome/Controllers/TodoListViewController.swift
@@ -187,6 +187,21 @@ extension TodoListViewContoller: UISearchBarDelegate {
         
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+       
+        if searchBar.text?.count == 0 {
+            
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+            
+        }
+        
+    }
+    
     
 }
 
