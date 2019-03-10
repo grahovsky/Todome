dbe47f0841f4beb40d0406213888e80c5ca828ac
Konstantin <grahovsky@gmail.com>
2019-03-10 11:11:17 +0300

    #delete data from Realm #Table View editingStyle

--- Todome/Controllers/TodoListViewController.swift
+++ Todome/Controllers/TodoListViewController.swift
@@ -89,6 +89,28 @@ class TodoListViewContoller: UITableViewController {
         
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            if let item = todoItems?[indexPath.row] {
+                
+                do {
+                    try realm.write {
+                        realm.delete(item)
+                    }
+                } catch {
+                    print("Error delete status, \(error.localizedDescription)")
+                }
+                
+            }
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
     
     // MARK: - Add new Items
     
